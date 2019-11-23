393a0cd3ccd5e8b2707b4531213581bae64042f1
asalimil <43479233+asalimil@users.noreply.github.com>
2019-11-23 03:59:30 -0700

    Update LMI_linear_programming.m

--- LMI_linear_programming.m
+++ LMI_linear_programming.m
@@ -1,1 +1,25 @@
+%% LMI for Linear Programming
+clc; clear; close all;
 
+a = [2 +1
+     3 -2
+     -1 2];
+b = [-1 -2 1]';
+c = [2  3]';
+
+x = sdpvar(2,1);
+
+t = c'*x;
+M1 = a(1,:)*x - b(1);
+M2 = a(2,:)*x - b(2);
+M3 = a(3,:)*x - b(3);
+
+Constraint = [];
+F = [Constraint, M1 <= 0; M2 <= 0; M3 <= 0];
+optimize(F,t);
+
+% minimized t
+t = value(t)
+
+% x
+x = value(x)
